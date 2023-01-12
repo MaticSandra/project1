51372dfa969a58a0f76e2ca0a66411c00e310533
MaticSandra <sandraandjelamatic@gmail.com>
2023-01-12 20:46:33 +0100

    Dodat toString

---
+++ src/main/java/zadatak1/Test.java
@@ -0,0 +1,31 @@
+package zadatak1;
+
+public class Test {
+
+    String nazivTesta;
+    double trajanjeTesta;
+
+    public String getNazivTesta() {
+        return nazivTesta;
+    }
+
+    public void setNazivTesta(String nazivTesta) {
+        this.nazivTesta = nazivTesta;
+    }
+
+    public double getTrajanjeTesta() {
+        return trajanjeTesta;
+    }
+
+    public void setTrajanjeTesta(double trajanjeTesta) {
+        this.trajanjeTesta = trajanjeTesta;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "nazivTesta='" + nazivTesta + '\'' +
+                ", trajanjeTesta=" + trajanjeTesta +
+                '}';
+    }
+}
